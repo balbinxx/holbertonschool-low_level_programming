13e74115f17cdc61dc018dc123e31033ecf107e0
balbinxx <1815@holbertonschool.com>
2020-06-12 04:39:42 +0000

    990

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * use of putchar to write characters specified by the argument
+ * Return: Always 0
+c */
+int main(void)
+{
+short al;
+for (al = '0'; al <= '9'; al++)
+putchar(al);
+putchar('\n');
+return (0);
+}
