cfce6c0d2495aee50e5405980a9c258058e34b38
balbinxx <1815@holbertonschool.com>
2020-06-19 15:24:33 +0000

    CRS

---
+++ 0x04-more_functions_nested_loops/0-isupp.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _isupper - chec uppercase character
+ * @x: character value
+ * Return: 1 (True) 0 (False)
+ */
+int _isupper(int x)
+{
+     return (x >= 'A' && x <= 'Z');
+}
