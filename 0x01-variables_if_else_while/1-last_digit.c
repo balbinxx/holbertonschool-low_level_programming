ea262b5887c0161d9edaa797b1f752ebb78969c4
balbinxx <1815@holbertonschool.com>
2020-09-17 21:35:13 +0000

    last instead n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,12 +10,9 @@ int main(void)
 {
 	int n;
 	int last;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	last = n % 10;
-
 	if ( last > 5 ) {
 	printf("Last digit of: %d is %d and is greater than 5\n", n, last);
 	}
@@ -24,6 +21,5 @@ int main(void)
 	}
 	else
 	printf("last digit of: %d is %d and is less than 6 and not 0", n, last);
-
 	return 0;
 }
