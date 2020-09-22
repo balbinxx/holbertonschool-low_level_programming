9810ec15959078074f292dcc075166107eb77403
balbinxx <1815@holbertonschool.com>
2020-09-22 21:58:26 +0000

    Alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
  * print_alphabet - prints alphabet in lowercase
- *
  * Return: nothing
  */
 void print_alphabet(void)
 {
 	char x;
+
 	for (x = 'a'; x <= 'z'; x++)
 	{
 		_putchar(x);
