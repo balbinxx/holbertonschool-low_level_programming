4949635d46f1093de8b82b787ac48c4acb91e043
balbinxx <1815@holbertonschool.com>
2020-09-22 23:54:51 +0000

    Check letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,15 +7,12 @@
  */
 int _isalpha(int c)
 {
-	if (c > 65 && c < 91)
+	if (c > 65 && c < 122)
 	{
-		else if (c > 96 && c < 122)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
