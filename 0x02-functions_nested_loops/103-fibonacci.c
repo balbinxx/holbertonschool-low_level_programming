bc38725145e5e4090eb57cabeccd515f32d1b871
balbinxx <1815@holbertonschool.com>
2020-09-23 03:10:45 +0000

    FIB 2nd

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - fibonacci
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 1, y = 2, res = 0;
+	int z;
+
+	while (y < 4000000)
+	{
+		if (y % 2 == 0)
+			res += y;
+
+		z = y;
+		y += x;
+		x = z;
+	}
+	printf("%d\n", res);
+	return (0);
+}
