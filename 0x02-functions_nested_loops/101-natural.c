e7ecfb5b1d554cb9f7a6d231dd2871d406d1b6ea
balbinxx <1815@holbertonschool.com>
2020-09-23 03:14:48 +0000

    IDK

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - adds multiples
+ * Return: void
+ */
+int main(void)
+{
+	int x = 1;
+	int y = 0;
+
+	while (x < 1024)
+	{
+		if (x % 3 == 0)
+			y += i;
+		else if (x % 5 == 0)
+			y += i;
+		x++;
+	}
+	printf("%d\n", y);
+
+	return (0);
+}
