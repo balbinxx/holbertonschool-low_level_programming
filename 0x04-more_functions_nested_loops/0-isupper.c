37f25ba46ccb853dd30c3c8ba7eae1b2cf2e6421
balbinxx <1815@holbertonschool.com>
2020-09-24 22:19:11 +0000

    Upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _isupper - check uppercase char
+ * @c: char to value
+ * Return: 1 (True) 0 (False)
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
