56ecb2001d4ad8a4beb35d116a6f47e3c65bd82a
balbinxx <1815@holbertonschool.com>
2020-09-25 03:29:21 +0000

    If is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _isdigit - checks if char is digit
+ * @c: char to check
+ * Return: 1 if it is a digit, 0 if is not
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
