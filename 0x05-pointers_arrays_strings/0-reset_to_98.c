c29dee9bd142e33c4d46efa7eab24f51b7c23026
balbinxx <1815@holbertonschool.com>
2020-09-29 22:40:04 +0000

    Keep it simple

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Updates the value of n into 98
+ * @n: pointer to an int to be updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
