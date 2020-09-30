1397f31f60ef041cd9cbc952cb655c609910c6ae
balbinxx <1815@holbertonschool.com>
2020-09-30 01:54:15 +0000

    Pr String

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _puts - prints a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
