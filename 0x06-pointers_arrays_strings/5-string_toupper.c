5e4f11e22572a172d0ac056950d37e994afa4777
balbinxx <1815@holbertonschool.com>
2020-10-02 04:12:17 +0000

    Lower to Upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - converts strings to uppercase
+ * @s: the string to convert
+ *
+ * Return: char pointer
+ */
+char *string_toupper(char *s)
+{
+	char *lsf = s;
+
+	for (; *s; s++)
+		if (*s >= 'a' && *s <= 'z')
+			*s += 'A' - 'a';
+	return (lsf);
+}
