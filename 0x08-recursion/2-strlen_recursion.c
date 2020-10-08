391215fab1f761e210b12ce98358f874268ddb8e
balbinxx <1815@holbertonschool.com>
2020-10-08 04:33:30 +0000

    Lenght of STt

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - return the lenght of a string
+ * @s: string to size lenght
+ * Return: integer/lenght of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return(0);
+}
