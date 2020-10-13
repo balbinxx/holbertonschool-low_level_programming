15972c44bf70e822e4f6648abda4fc3f7c002f79
balbinxx <1815@holbertonschool.com>
2020-10-13 21:44:33 +0000

    Arr of Chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  * @c: char of initialitation
  * Return: Pointer to the array
  */
-char create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
         unsigned int i;
 	char *ptr = NULL;
