a3c766660bff84b6e80665bf5e1412f31e8798ca
balbinxx <1815@holbertonschool.com>
2020-10-16 03:34:02 +0000

    n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,18 @@
  * Return: None
  */
 void print_to_98(int n)
+	void print_to_98(int n)
 {
-	int i = n;
-
-	if (i <= 98);
-
-	for (i = n; i <= 98; i++)
+	while (n < 98)
 	{
-		printf("%i, " , i);
+		printf("%d, ", n);
+		n++;
 	}
-	if (i > 98);
-
-	for (i = n; i >= 98; i--)
+	while (n > 98)
 	{
-		printf("%i, " , i);
+		printf("%d, ", n);
+		n--;
 	}
+	printf("98\n");
+
 }
