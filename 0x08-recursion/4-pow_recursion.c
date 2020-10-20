6850f5e2b27ba93e74bdcc0519cff0c61eaa3b20
balbinxx <1815@holbertonschool.com>
2020-10-20 21:50:32 +0000

    Power REC

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: the base
+ * @y: the exponent
+ *
+ * Return: the int or -1 error
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	return (1);
+}
