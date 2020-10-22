485914466138251f5c0a3b1671fe82637fce4402
balbinxx <1815@holbertonschool.com>
2020-10-22 03:03:10 +0000

    PR a Name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers-h"
+
+/**
+ * print_name - print a name
+ * @name: string name
+ * @f: pointer to function
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+
+{
+	if (name && f)
+		f(name);
+}
