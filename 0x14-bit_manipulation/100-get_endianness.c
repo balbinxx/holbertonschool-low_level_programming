1ebc2494f9532f4cb43286ebd683f318cbf54435
balbinxx <1815@holbertonschool.com>
2020-11-06 02:23:36 +0000

    endianess

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned long int x = 1;
+
+	return (*(char *)&x);
+}
