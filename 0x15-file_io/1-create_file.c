4a1e726e3982b248900cc24a59e73463bfce4b6d
balbinxx <1815@holbertonschool.com>
2020-11-10 04:32:56 +0000

    A/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,11 @@ int create_file(const char *filename, char *text_content)
 	int t = 0; 
 	int r = 0;
 
-	
-	if (text_content == NULL)
-		return (-1);
 	if (filename == NULL)
 		return (-1);
-	
+	if (text_content == NULL)
+		return (-1);
+
 	w = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (w == -1)
 		return (-1);
