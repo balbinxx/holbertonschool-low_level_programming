6e65b4a64db978295a72c248649d238f399be441
balbinxx <1815@holbertonschool.com>
2020-11-10 04:32:00 +0000

    A/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,15 +18,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	
 	w = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
-	
+	if (w == -1)
+		return (-1);
 	if (text_content)
 	{
 		while (text_content[r] != '\0')
 			r++;
 		t = write(w, text_content, r);
 	}
-	if (w == -1)
-		return (-1);
 	if (t == -1 || t != r)
 		return (-1);
 	close(w);
