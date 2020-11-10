19d3fb967951d13c174198b1530a35aa8fb22ed1
balbinxx <1815@holbertonschool.com>
2020-11-10 04:31:15 +0000

    A/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * create_file - name
+ * @filename: char pointer
+ * @text_content: char pointer
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int w = 0; 
+	int t = 0; 
+	int r = 0;
+
+	
+	if (text_content == NULL)
+		return (-1);
+	if (filename == NULL)
+		return (-1);
+	
+	w = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	
+	if (text_content)
+	{
+		while (text_content[r] != '\0')
+			r++;
+		t = write(w, text_content, r);
+	}
+	if (w == -1)
+		return (-1);
+	if (t == -1 || t != r)
+		return (-1);
+	close(w);
+	return (1);
+}
